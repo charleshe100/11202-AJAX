f23cf3fbbc357c96cdd768619b3de43531f096f2
charles100 <brs-68@yahoo.com.tw>
2024-06-18 23:40:50 +0800

    app2, 新增學生資料欄位，不同的資料表同時新增資料

--- app2/api/delete.php
+++ app2/api/delete.php
@@ -1,7 +1,17 @@
 <?php
 include_once "db.php";
 //處理刪除資料的請求
+// if(isset($_POST['id'])){
+//     echo ($_POST['id']);
+// }else{
+//     echo "找不到id！";
+// };
+// exit();
 if(isset($_POST['id'])){
     $Student->del($_POST['id']);
+    // $ClassStudent->del($_POST['id']);
+    // $sql="SELECT `student_scores`.`school_num` FROM `students` INNER JOIN `student_scores` ON `students`.`school_num` = `student_scores`.`school_num` WHERE `id`='{$_POST['id']}'";
+    // $schoolnum=$Student->q($sql);
+    // $StudentScores->q("DELETE FROM `student_scores` WHERE school_num='{$schoolnum}'");
 }
 ?>
